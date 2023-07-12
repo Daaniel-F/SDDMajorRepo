2450cc346cd938f55ec9026f2096d825e0343874
Daaniel-F <137018268+Daaniel-F@users.noreply.github.com>
2023-07-12 13:01:28 +1000

    Add all pages
    Search page
    
    Library page
    
    Profile page

---
+++ SDDMajorWorkGit/SDDMajorWorkGit/LibraryPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SDDMajorWorkGit;
+
+public partial class LibraryPage : ContentPage
+{
+	public LibraryPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SDDMajorWorkGit/SDDMajorWorkGit/SearchPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SDDMajorWorkGit;
+
+public partial class SearchPage : ContentPage
+{
+	public SearchPage()
+	{
+		InitializeComponent();
+	}
+}
