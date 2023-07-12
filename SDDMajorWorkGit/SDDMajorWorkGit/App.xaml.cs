6a5024149f24d3dc2d1337b1c5acbbaee49a8770
Daaniel-F <137018268+Daaniel-F@users.noreply.github.com>
2023-07-12 16:33:30 +1000

    Add basic database functionality

--- SDDMajorWorkGit/SDDMajorWorkGit/App.xaml.cs
+++ SDDMajorWorkGit/SDDMajorWorkGit/App.xaml.cs
@@ -2,6 +2,21 @@
 
 public partial class App : Application
 {
+	private static Database database;
+
+	public static Database Database
+	{
+		get
+		{
+			if (database == null)
+			{
+				database = new Database(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "songs.db3"));
+			}
+
+			return database;
+		}
+	}
+
 	public App()
 	{
 		InitializeComponent();
---
+++ SDDMajorWorkGit/SDDMajorWorkGit/Song.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SQLite;
+namespace SDDMajorWorkGit
+{
+	public class Song
+	{
+		[PrimaryKey, AutoIncrement]
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Artist { get; set; }
+	}
+}
+
