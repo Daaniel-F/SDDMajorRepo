d434e04b2f4a5be5c8fa9a3e9328526af4b140da
Daaniel-F <137018268+Daaniel-F@users.noreply.github.com>
2023-07-23 13:21:43 +1000

    Add search bar functionality
    songList Collection view filters results based on search bar value

--- SDDMajorWorkGit/SDDMajorWorkGit/SearchPage.xaml.cs
+++ SDDMajorWorkGit/SDDMajorWorkGit/SearchPage.xaml.cs
@@ -12,4 +12,19 @@ public partial class SearchPage : ContentPage
 		//Collection view gets content from SongRepo
 		songList.ItemsSource = App.SongRepo.GetAllSongs();
 	}
+
+	//Runs when search bar text is changed
+	void SearchBar_TextChanged(System.Object sender, Microsoft.Maui.Controls.TextChangedEventArgs e)
+    {
+		if (string.IsNullOrWhiteSpace(e.NewTextValue))
+		{
+            //Displays all entries in SongRepo when search bar is empty
+            songList.ItemsSource = App.SongRepo.GetAllSongs();
+		}
+		else
+		{
+			//Displays entries in SongRepo that contain the search value in the Name column
+			songList.ItemsSource = App.SongRepo.GetAllSongs().Where(i => i.Name.ToLower().Contains(e.NewTextValue.ToLower()));
+		}
+    }
 }
